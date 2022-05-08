f48b5f19acc6394507ac008ab4a3fc05b8d094d6
YanisHlali <yanis.hlali@gmail.com>
2022-05-08 19:03:37 +0200

    Modification About page experience

--- src/mfr_de_vif/About_3.js
+++ src/mfr_de_vif/About_3.js
@@ -53,19 +53,22 @@ export default function About() {
 
     return(
         <Grid direction="row" container justify="center" alignItems="center" className={classes.cont}>
-            <Grid item xs={12} lg={6}>
-                <Typography variant="h2" gutterBottom component="p">
-                    Schéma de base de données
-                </Typography>            
-            <Grid container direction="column" item xs={12} lg={6} spacing={2} justify="center" alignItems="center">
-                <Image
-                    alt="Skills"
-                    src="/bdd_stage1.png"
-                    width="1139"
-                    height="655"
-                />              
-            </Grid>
-            </Grid>
+        <Grid item xs={12} lg={6}>
+            <Typography variant="h2" gutterBottom component="p">
+            Schéma de base de données
+            </Typography>
+            <Typography variant="h6" gutterBottom component="p">
+                {about.stage1.mission}
+            </Typography>                
         </Grid>
+        <Grid container direction="column" item xs={12} lg={6} spacing={2} justify="center" alignItems="center">
+        <Image
+            alt="Skills"
+            src="/bdd_stage1.png"
+            width="1139"
+            height="655"
+        />             
+        </Grid>
+    </Grid>
     )
 }
